ba6710cffc590a8298b67f968e2a68547a306f93
songtn2002 <1936535038@qq.com>
2018-12-22 09:28:51 +0800

    "commit 12/20/18 TreenetworkCore&MatrixFetch Passed"

--- src/MatrixFetch.java
+++ src/MatrixFetch.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class MatrixFetch {
@@ -10,37 +10,47 @@ public class MatrixFetch {
     private static int M;
     private static int[][] matrix;
 
-    private static int max;
+    private static BigInteger max = BigInteger.ZERO;
 
-    private static int calcMax(int[] line){
-        int[][] dp = new int[M][M];
-        int coeff = 1;
+    private static BigInteger calcMax(int[] line){
+        BigInteger[][] dp = new BigInteger[M][M];
+        for (int i = 0; i<M; i++){
+            for (int j = 0; j<M; j++){
+                dp[i][j] = BigInteger.ZERO;
+            }
+        }
+        BigInteger coeff = BigInteger.ONE;
         for (int i = 1; i < M; i++){
-            coeff *=2;
+            coeff = coeff.multiply(BigInteger.valueOf(2));
             for (int j = 0; j<=i; j++){
                 int st = j;
                 int nd = j+(M-1-i);
-                int fstOp = 0;
-                if (nd<M-1) fstOp = dp[i-1][j]+line[nd+1]*coeff;
-                int sndOp = 0;
-                if (j!=0) sndOp = dp[i-1][j-1]+line[st-1]*coeff;
-                dp[i][j] = Math.max(fstOp, sndOp);
+                BigInteger fstOp = BigInteger.ZERO;
+                if (nd<M-1) fstOp= dp[i-1][j].add(coeff.multiply(BigInteger.valueOf(line[nd+1])));
+                BigInteger sndOp = BigInteger.ZERO;
+                if (j!=0) sndOp = dp[i-1][j-1].add(BigInteger.valueOf(line[st-1]).multiply(coeff));
+                //System.out.println(fstOp.toString()+"+"+sndOp.toString());
+                if (fstOp.compareTo(sndOp)<0){
+                    dp[i][j] = sndOp;
+                }else{
+                    dp[i][j] = fstOp;
+                }
             }
         }
         //System.out.println("IN: "+Arrays.toString(line));
-        //for (int[] a: dp)System.out.println(Arrays.toString(a));
+        //for (BigInteger[] a: dp)System.out.println(Arrays.deepToString(a));
         //System.out.println("------------------------------");
-        int ans = 0;
-        coeff *=2;
+        BigInteger ans = BigInteger.ZERO;
+        coeff = coeff.multiply(BigInteger.valueOf(2));
         for (int i = 0; i<M; i++){
-            int score = dp[M-1][i]+line[i]*coeff;
-            if (score>ans) ans = score;
+            BigInteger score = dp[M-1][i].add(coeff.multiply(BigInteger.valueOf(line[i])));
+            if (score.compareTo(ans) > 0) ans = score;
         }
         return ans;
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream ("MatrixFetch.in")));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(in.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
@@ -53,8 +63,8 @@ public class MatrixFetch {
         }
         in.close();
         for (int[] line: matrix){
-            max += calcMax(line);
+            max = max.add(calcMax(line));
         }
-        System.out.println(max);
+        System.out.println(max.toString());
     }
 }
